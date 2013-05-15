f869b66721fbc45660f58e14be9d5a39f11e5d2b
KIKIJIKI <dev@kikijiki.com>
2013-05-16 00:15:19 +0900

    ライブラリの追加

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ MONOGATARI
 * Extended markdown syntax ([php-markdown], [php-smartypants])
 * Syntax highliting ([prettify])
 * Localization (two languages)
-* Caching of rendered markdown pages
-* Support for themes
+* Caching of rendered markdown pages ([zf2])
+* Support for themes ([twig])
 * Simple and short single-file application
 
 [php-markdown]: https://github.com/michelf/php-markdown
 [php-smartypants]: https://github.com/michelf/php-smartypants
 [prettify]: https://code.google.com/p/google-code-prettify/‎
+[zf2]: https://github.com/zendframework/zf2
+[twig]: https://github.com/fabpot/Twig
--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 define('ROOT_DIR', (realpath(dirname(__FILE__)) == '/' ? '/' : realpath(dirname(__FILE__)) . '/'));
 define('CONTENT_DIR', ROOT_DIR . 'content/');
 define('ENGINE_DIR', ROOT_DIR . 'engine/');
+define('LIB_DIR', ROOT_DIR . 'lib/');
 define('LAYOUT_DIR', ROOT_DIR . 'layout/');
 
 require(ENGINE_DIR . 'Monogatari.php');
 
-require_once(ENGINE_DIR . 'markdown.php');
-require_once(ENGINE_DIR . 'markdownExtra.php');
-require_once(ENGINE_DIR . 'smartypants.php');
-require_once(ENGINE_DIR . 'twig/Autoloader.php');
-require_once(ENGINE_DIR . 'zend/Loader/StandardAutoloader.php');
+require_once(LIB_DIR . 'php-markdown/Michelf/markdown.php');
+require_once(LIB_DIR . 'php-markdown/Michelf/markdownExtra.php');
+require_once(LIB_DIR . 'php-smartypants/smartypants.php');
+require_once(LIB_DIR . 'twig/lib/Twig/Autoloader.php');
+require_once(LIB_DIR . 'zf2/library/Zend/Loader/StandardAutoloader.php');
 
 $l =  new Zend\Loader\StandardAutoloader(array(
 	'autoregister_zf' => true,
