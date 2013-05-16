2bd2dc6804b8451c61726f6a3595ae3d99d5544f
KIKIJIKI <dev@kikijiki.com>
2013-05-16 20:05:36 +0900

    おっと

--- settings.php
+++ settings.php
@@ -4,10 +4,13 @@ $settings['site_title']['en'] = 'MONOGATARI';
 $settings['site_title']['ja'] = '物語';
 $settings['continue_reading']['en'] = '[continue reading]';
 $settings['continue_reading']['ja'] = '【読み続く】';
+
 $settings['base_url'] = '/';
 $settings['layout'] = 'somatsu';
-$settings['analytics_ua'] = null;
-$settings['cache_options'] = array('cache_dir' => ROOT_DIR.'cache'); //false to disable, check zf2 for more options
+
+//false to disable, check the zf2 documentation for more options.
+$settings['cache_options'] = array('cache_dir' => ROOT_DIR.'cache');
+$settings['analytics_ua'] = false;
 $settings['twig_config'] = array(
     'cache' => false,
     'autoescape' => false,
